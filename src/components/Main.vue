509c8eaae721ccd7210c86a4f9ad7c7fbc440ace
TomasBeguan <TomasBeguan>
2021-09-29 15:35:10 -0300

    Filtro de busqueda

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,29 +1,132 @@
 <template>
-  <v-container class="container_cards">
+
+  <v-container class="container_cards" id="app">
+
+        <!-- NAVIGATION DRAWER -->
+        <v-navigation-drawer
+          v-model="drawer"
+          app
+          
+        >
+        <v-card>
+          <v-card-title>
+            Filters
+          </v-card-title>
+        </v-card>
+        <v-expansion-panels>
+          <v-expansion-panel
+            v-for="(item,i) in 10"
+            :key="i"
+          >
+              <v-expansion-panel-header>
+                Skin
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-checkbox
+                  v-model="selected"
+                  label="Honey"
+                  value="Honey"
+                ></v-checkbox>
+                <v-checkbox
+                  v-model="selected"
+                  label="Almond"
+                  value="Almond"
+                ></v-checkbox>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-navigation-drawer>
+        <!-- NAVIGATION DRAWER -->
+
+        <!-- APP BAR -->
+            <v-app-bar app>
+            
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>PUNKY GANG</v-toolbar-title>
+
+          <div class="text-center">
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  color="primary"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  Rarity: High to Low
+                  <v-icon
+                    right
+                    dark
+                  >
+                    mdi-chevron-down
+                  </v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in items"
+                  :key="index"
+                  link
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </div>
+
+
+          <v-btn
+          rounded
+          color="primary"
+          dark
+          target="_blank"
+          href="https://davinci.gallery/artist/punkygang"
+          >
+            Davinci
+          </v-btn>
+
+
+        </v-app-bar>
+        <!-- NAVIGATION DRAWER -->
+    
+        <div class="buscador_div">
+            <v-text-field
+                v-model="search"
+                label="Search your Punky"
+                solo
+                clearable
+                prepend-inner-icon="mdi-magnify"
+                class=""
+            ></v-text-field>
+        </div>
+    
     <div
-    v-for="punky in punkys" :key="punky.DocID"  
+    v-for="item in filteredList" :key="item.DocID"  
     class="card_punky"
     >
     <v-card
     class="card_inside border_radius_a"
-    max-width="250"
+    max-width="300"
+    v-bind:href="item.url" 
+    target="_blank"
     >
 
       <v-img
         height="250"
         width="250"
-        src="https://davinci.gallery/uploads/artwork/Qmb3PWjkVb5YAbjJjdbPyLJ96TMSkT3MeGJZaDTW8AYrCf"
+        :src=item.img
         class="border_radius_a"
       ></v-img>
 
       <h1
       class="text-center"
       >
-      #129
+      #{{item.id}}
       </h1>
 
       <h2 class="text-center">
-        Lander {{ punky.mensaje }}
+        {{ item.nombre }}
       </h2>
 
 
@@ -33,49 +136,65 @@
 </template>
 
 <script>
+
+import datos from "../assets/json/punkys.json";
+
   export default {
     name: 'Main',
 
+    props: {
+    msg: String
+    }, 
+
     data: () => ({
-          punkys: [
-          { mensaje: 'Foo' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-          { mensaje: 'Bar' },
-        ]
+        drawer: null,
+        search: ''
     }),
 
     methods: {
       
     },
 
+    computed: {
+    items() {
+      return datos.map((item) => {
+        return item;
+      })
+    },
+    filteredList2() {
+      return this.items.filter(item => {
+        return item.nombre.toLowerCase().includes(this.search.toLowerCase())
+      })
+    },
+    filteredList() {
+        const value= this.search.charAt(0).toUpperCase() + this.search.slice(1);
+        return this.items.filter(function(item){
+          return item.nombre.indexOf(value) > -1 ||
+                 item.id.indexOf(value) > -1 ||
+                 item.id.indexOf(value) > -1
+        })
+    }
+  }
+
   }
 </script>
 
 <style lang="sass">
+.v-main__wrap
+    background-color: #DF57FF
 .container_cards
     display: flex
     flex-wrap: wrap
     justify-content: center
-    background-color: #DF57FF
 .card_punky
     max-width: 300px
     margin: 10px
-    
+    cursor: pointer
 .card_inside
     padding: 10px
+
+.buscador_div
+    width:100%
 
 
 .border_radius_a
